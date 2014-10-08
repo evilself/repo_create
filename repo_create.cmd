c74256af14693361c59cd63d5953d9a1aca4a386
evilself <borismechkov@gmail.com>
2014-10-07 19:01:17 -0500

    Added parameters to the batch script

--- repo_create.cmd
+++ repo_create.cmd
@@ -1,13 +1,20 @@
 @ECHO OFF
 REM **********************************************************************************************
-REM CREATE A GITHUB REPOSITORY REMOTELY
+REM PURPOSE:  CREATES A GITHUB REPOSITORY REMOTELY
+REM USATE  :  REPO_CREATE <username> <auth_token> <repo_name> <description>
+REM AUTHOR :  BORIS M
+REM DATE   :  10.7.2014
+REM TOKEN  :  eb85f416ef5ac955cc847dae6fc64584ae4255aa
 REM **********************************************************************************************
 SET VER=1.0
 SET V=********************************************************************************************
 IF "%1" == "" GOTO NOARGS
-ECHO CREATING "%1" REPOSITORY...
-CURL -u "evilself:eb85f416ef5ac955cc847dae6fc64584ae4255aa" https://api.github.com/user/repos -d "{\"name\":\"%1\"}"
+IF "%2" == "" GOTO NOARGS
+IF "%3" == "" GOTO NOARGS
+IF "%4" == "" GOTO NOARGS
+
+ECHO CREATING "%3" REPOSITORY...
+CURL -u "%1:%2" https://api.github.com/user/repos -d "{\"name\":\"%3\",\"description\":\"%4\"}"
 ECHO CREATED!
 
 :NOARGS
-EXIT 1
